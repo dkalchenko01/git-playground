8cf7bdbe057db7d7a9b990f8ff6ff6621a5b5114
dkalchenko01 <dkalchenko@kse.org.ua>
2024-10-29 13:34:02 +0200

    checking if guess already exists

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ while not is_game_over():
     if not guess_is_valid(guess):
         continue
 
+    if guess in guesses:
+        print('You have already made up this word')
+        continue
+
     if guess in full_list:
         guessed += 1
         guesses.append(guess)
